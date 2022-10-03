12677ef2fbc352c66cbe1b1a8a3e650f9afbcc7a
faza <faza@gitopia.org>
2022-10-03 11:41:09 +0530

    Initialize keyring in client context

--- cmd/git-gitopia/main.go
+++ cmd/git-gitopia/main.go
@@ -6,11 +6,15 @@ import (
 	"os"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/keys"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
+	sdkkeyring "github.com/cosmos/cosmos-sdk/crypto/keyring"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/spf13/cobra"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 const (
@@ -22,7 +26,7 @@ const (
 func main() {
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, client.ClientContextKey, new(client.Context))
-	version.Name = AppName // os keyring service name is same as version name 
+	version.Name = AppName // os keyring service name is same as version name
 	cmd := &cobra.Command{
 		Use:               "gitopia",
 		CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
@@ -30,7 +34,24 @@ func main() {
 			conf := sdk.GetConfig()
 			conf.SetBech32PrefixForAccount(AccountAddressPrefix, AccountAddressPrefix+sdk.PrefixPublic)
 			conf.Seal()
-			initClientCtx := client.Context{}.WithInput(os.Stdin)
+
+			registry := codectypes.NewInterfaceRegistry()
+			cryptocodec.RegisterInterfaces(registry)
+			kb, err := cmd.Flags().GetString(flags.FlagKeyringBackend)
+			if err != nil {
+				return err
+			}
+			kd, err := cmd.Flags().GetString(flags.FlagKeyringDir)
+			if err != nil {
+				return err
+			}
+			k, err := sdkkeyring.New(AppName, kb, kd, os.Stdin, codec.NewProtoCodec(registry))
+			if err != nil {
+				return err
+			}
+
+			initClientCtx := client.Context{}.WithInput(os.Stdin).WithKeyring(k)
+
 			// sets global flags for keys subcommand
 			return client.SetCmdClientContextHandler(initClientCtx, cmd)
 		},
