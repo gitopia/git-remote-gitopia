554f2806c69b012b69a6347511be48b87f1bee55
faza <faza@gitopia.org>
2022-11-14 21:06:38 +0530

    Use the correct node rpc address

--- config/config_prod.go
+++ config/config_prod.go
@@ -5,6 +5,6 @@ package config
 const (
 	GRPCHost      = "grpc.gitopia.com:9090"
 	GitServerHost = "https://server.gitopia.com"
-	TmAddr        = "rpc.gitopia.com:26657"
-	GasPrices     = "0.001ulore"
+	TmAddr        = "https://rpc.gitopia.com:443"
+	GasPrices     = "0.001utlore"
 )
